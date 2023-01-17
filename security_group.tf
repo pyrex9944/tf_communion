2c0cfab8bfeb7eede27515ee407adeef8af9b022
ashirov <iprochechya@gmail.com>
2023-01-17 21:14:48 +0500

    remove icmp

--- security_group.tf
+++ security_group.tf
@@ -18,14 +18,6 @@ resource "aws_security_group" "staging_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    description = "ALL ICMP - IPv4"
-    from_port   = -1    
-    to_port     = -1
-    protocol    = "ICMP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
 
   egress {
     from_port   = 0
